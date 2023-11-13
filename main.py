8b52a2a6aed6b15d0f59c2ebed6fbd22a0e35b33
Sunnyshineshow <sun.napon11@gmail.com>
2023-11-13 17:19:42 +0700

    แรงงานเจสัน

--- main.py
+++ main.py
@@ -1,16 +1,92 @@
+import pygame, sys, math
+from pygame import mixer
+from src.Utility.constants import *
+
 from src.GameObjects.Card.CardDrawer import CardDrawer
 from src.GameObjects.Board.Board import Board
 from src.GameObjects.Hand.Hand import Hand
 from src.GameObjects.Quest.QuestPool import QuestPool
 from src.GameState.StateMachine import StateMachine
+from src.GameState.StartState import StartState
+
+
+pygame.mixer.pre_init(44100, -16, 2, 4096)
+pygame.init()
+
+music_channel = mixer.Channel(0)
+music_channel.set_volume(0.2)
 
 
 class MainGame:
     def __init__(self):
+        self.max_frame_rate = 60
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+        self.bg_image = pygame.image.load("./graphics/background.png")
+
+        self.bg_image = pygame.transform.scale(self.bg_image, (WIDTH + 5, HEIGHT + 5))
+
+        self.num_dup_images = math.ceil(WIDTH / self.bg_image.get_width()) + 1
+        # print(self.bg_image.get_size(), self.num_dup_images)
+        self.scroll = 0
+        self.scroll_bg = False
+
         self.g_state_manager = StateMachine(self.screen)
         # Import the game state here
-        states = {}
+        states = {
+            "start": StartState(self.g_state_manager),
+        }
         self.g_state_manager.SetStates(states)
+
+    def RenderBackground(self):
+        if self.scroll_bg:
+            i = 0
+            while i < self.num_dup_images:
+                main.screen.blit(
+                    self.bg_image, (self.bg_image.get_width() * i + self.scroll, 0)
+                )  # appending same images to the back
+                i += 1
+            self.scroll -= 6
+            if abs(self.scroll) > self.bg_image.get_width():
+                self.scroll = 0
+        else:
+            main.screen.blit(self.bg_image, (0, 0))
+
+    def PlayGame(self):
+        # self.bg_music.play(-1)
+        clock = pygame.time.Clock()
+        self.g_state_manager.Change(
+            "start",
+            {},
+        )
+
+        scroll = 0
+
+        while True:
+            pygame.display.set_caption(
+                "breakout game running with {:d} FPS".format(int(clock.get_fps()))
+            )
+            dt = clock.tick(self.max_frame_rate) / 1000.0
+
+            # input
+            events = pygame.event.get()
+
+            # update
+            self.g_state_manager.update(dt, events)
+
+            # bg render
+            self.RenderBackground()
+            # render
+            self.g_state_manager.render()
+
+            # screen update
+            pygame.display.update()
+
+
+if __name__ == "__main__":
+    main = MainGame()
+
+    main.PlayGame()
 
 
 # print(QuestPool().random_quest())
---
+++ src/GameState/StartState.py
@@ -0,0 +1,71 @@
+from src.GameState.BaseState import BaseState
+import pygame, sys
+from src.Utility.constants import *
+
+
+class StartState(BaseState):
+    def __init__(self, state_manager):
+        super(StartState, self).__init__(state_manager)
+        # start = 1,       ranking = 2
+        self.option = 1
+
+        self.menu_change_sound = pygame.mixer.Sound("sounds/paddle_hit.wav")
+        # self.confirm_sound = pygame.mixer.Sound("sounds/confirm.wav")
+        self.medium_font = pygame.font.Font("./fonts/font.ttf", 48)
+        self.large_font = pygame.font.Font("./fonts/font.ttf", 96)
+
+    def Exit(self):
+        pass
+
+    def Enter(self, params):
+        # self.high_scores = params["high_scores"]
+        pass
+
+    def update(self, dt, events):
+        for event in events:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+                if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    if self.option == 1:
+                        self.option = 2
+                    else:
+                        self.option = 1
+                    self.menu_change_sound.play()
+                # if event.key == pygame.K_RETURN:
+                #     self.confirm_sound.play()
+
+                #     if self.option == 1:
+                #         self.state_machine.Change(
+                #             "paddle-select", {"high_scores": self.high_scores}
+                #         )
+                #     else:
+                #         self.state_machine.Change(
+                #             "high-scores", {"high_scores": self.high_scores}
+                #         )
+
+    def render(self, screen):
+        # title
+        t_title = self.large_font.render("Game Reang Phai", False, (255, 255, 255))
+        rect = t_title.get_rect(center=(WIDTH / 2, HEIGHT / 3))
+        screen.blit(t_title, rect)
+
+        t_start_color = (255, 255, 255)
+        t_highscore_color = (255, 255, 255)
+
+        if self.option == 1:
+            t_start_color = (103, 255, 255)
+
+        if self.option == 2:
+            t_highscore_color = (103, 255, 255)
+
+        t_start = self.medium_font.render("START", False, t_start_color)
+        rect = t_start.get_rect(center=(WIDTH / 2, HEIGHT / 2 + 210))
+        screen.blit(t_start, rect)
+        t_highscore = self.medium_font.render("HIGH SCORES", False, t_highscore_color)
+        rect = t_highscore.get_rect(center=(WIDTH / 2, HEIGHT / 2 + 280))
+        screen.blit(t_highscore, rect)
